93077418a1e493919f0259b98c018ee86be8e423
2wndrhs <2wndrhs@gmail.com>
2024-08-19 01:34:29 +0900

    fix: outdated된 `layout` prop 제거

--- src/components/BannerCarousel.tsx
+++ src/components/BannerCarousel.tsx
@@ -1,12 +1,13 @@
 "use client";
-import { Swiper, SwiperSlide } from "swiper/react";
+
+import Image from "next/image";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
-import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
 import 추천조합1 from "../../public/추천조합1.svg";
 import 추천조합2 from "../../public/추천조합2.svg";
-import "./BannerCarousel.style.css"
+import "./BannerCarousel.style.css";
 
 export default function BannerCarousel() {
   return (
@@ -14,23 +15,13 @@ export default function BannerCarousel() {
       spaceBetween={30}
       pagination={{ clickable: true }}
       modules={[Pagination]}
-      className="w-full mt-2"
+      className="mt-2 w-full"
     >
       <SwiperSlide>
-        <Image
-          src={추천조합1}
-          alt="추천 조합 1"
-          layout="responsive"
-          className="rounded-lg"
-        />
+        <Image src={추천조합1} alt="추천 조합 1" className="rounded-lg" />
       </SwiperSlide>
       <SwiperSlide>
-        <Image
-          src={추천조합2}
-          alt="추천 조합 2"
-          layout="responsive"
-          className="rounded-lg"
-        />
+        <Image src={추천조합2} alt="추천 조합 2" className="rounded-lg" />
       </SwiperSlide>
     </Swiper>
   );
