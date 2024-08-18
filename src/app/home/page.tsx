3892280e546953cd8d823192e2bc5a03da9baf5a
2wndrhs <2wndrhs@gmail.com>
2024-08-19 04:46:21 +0900

    refactor: `BaseFlavorSelect` 컴포넌트 리팩토링

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import BannerCarousel from "@/components/BannerCarousel";
-import MenuSelector from "@/components/Base";
-import ItemSelector from "@/components/Topping";
+import BaseFlavorSelect from "@/components/home/BaseFlavorSelect";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 import ping from "../../../public/ping.svg";
+import 기본 from "../../../public/기본.svg";
+import 체리쉘 from "../../../public/체리쉘.svg";
+import 초코쉘 from "../../../public/초코쉘.svg";
+import { BaseFlavor, BaseSize } from "../types/common";
 import { ddag } from "../utils/fonts";
 
 interface Item {
@@ -15,50 +18,64 @@ interface Item {
 
 export default function Home() {
   const router = useRouter();
-  const [selectedMenu, setSelectedMenu] = useState<string | null>(null);
-  const [selectedGrams, setSelectedGrams] = useState<any>({});
-  const [selectedToppings, setSelectedToppings] = useState<Item[]>([]);
-  const [location, setLocation] = useState<string>("");
+  const searchParams = useSearchParams();
 
-  useEffect(() => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const address = queryParams.get("address");
+  const address = searchParams.get("address") ?? "";
 
-    if (address) {
-      setLocation(address);
-    }
-  }, []);
+  const [location, setLocation] = useState(address);
 
-  const handleSelectionChange = (menu: string, grams: any) => {
-    setSelectedMenu(menu);
-    console.log(menu);
-    setSelectedGrams(grams);
-    console.log(grams);
+  const handleSelectFlavor = (flavor: keyof typeof BaseFlavor) => {
+    console.log("Selected: ", flavor);
   };
 
-  const handleToppingSelectionChange = (toppings: Item[]) => {
-    setSelectedToppings(toppings);
-    console.log("토핑", toppings);
+  const handleSelectSize = (size: keyof typeof BaseSize) => {
+    console.log("Selected: ", size);
   };
 
-  const handleComplete = () => {
-    if (selectedMenu) {
-      const query = {
-        address: location,
-        menu: selectedMenu,
-        grams: JSON.stringify(selectedGrams),
-        toppings: JSON.stringify(selectedToppings),
-      };
-      router.push(`/result?${new URLSearchParams(query).toString()}`);
-    } else {
-      alert("메뉴를 선택해주세요.");
-    }
-  };
+  // const [selectedGrams, setSelectedGrams] =
+  //   useState<keyof typeof BaseSize>("1인 150g");
+  // const [selectedToppings, setSelectedToppings] = useState<Item[]>([]);
+
+  // useEffect(() => {
+  //   const queryParams = new URLSearchParams(window.location.search);
+  //   const address = queryParams.get("address");
+
+  //   if (address) {
+  //     setLocation(address);
+  //   }
+  // }, []);
+
+  // const handleSelectionChange = (menu: string, grams: any) => {
+  //   console.log("handleSelectionChange");
+  //   console.log("menu", menu);
+  //   console.log("grams", grams);
+  //   setSelectedMenu(menu);
+  //   setSelectedGrams(grams);
+  // };
+
+  // const handleToppingSelectionChange = (toppings: Item[]) => {
+  //   setSelectedToppings(toppings);
+  //   console.log("토핑", toppings);
+  // };
+
+  // const handleComplete = () => {
+  //   if (selectedMenu) {
+  //     const query = {
+  //       address: location,
+  //       menu: selectedMenu,
+  //       grams: JSON.stringify(selectedGrams),
+  //       toppings: JSON.stringify(selectedToppings),
+  //     };
+  //     router.push(`/result?${new URLSearchParams(query).toString()}`);
+  //   } else {
+  //     alert("메뉴를 선택해주세요.");
+  //   }
+  // };
 
   return (
-    <div className="h-screen p-4">
+    <div className="p-4">
       <header
-        className={`${ddag.variable} ali mb-4 mt-6 font-ddag text-2xl text-gray-800`}
+        className={`${ddag.variable} mb-4 mt-6 font-ddag text-2xl text-gray-800`}
       >
         요아소비빠따정
       </header>
@@ -70,20 +87,22 @@ export default function Home() {
           value={location}
           onChange={(e) => setLocation(e.target.value)}
         />
-        <Image
-          src={ping}
-          alt="아이콘"
-          width={24}
-          height={24}
-          className="ml-auto"
-        />
+        <Image src={ping} alt="아이콘" width={24} height={24} />
       </div>
 
-      <div className="mt-4 text-[24px] font-bold">나는 이게 조합</div>
+      <div className="mt-4 text-2xl font-bold">나는 이게 조합</div>
       <BannerCarousel />
-      <div className="mt-4 text-[24px] font-bold">나는 이런 조합</div>
+      <div className="mt-4 text-2xl font-bold">나는 이런 조합</div>
       <div className="mt-2 h-0.5 w-full bg-[#F63F5D]"></div>
-      <MenuSelector onSelectionChange={handleSelectionChange} />
+      <BaseFlavorSelect
+        onSelectFlavor={handleSelectFlavor}
+        onSelectSize={handleSelectSize}
+      >
+        <BaseFlavorSelect.Option flavor="요거트아이스크림" imgSrc={기본} />
+        <BaseFlavorSelect.Option flavor="초코쉘" imgSrc={초코쉘} />
+        <BaseFlavorSelect.Option flavor="체리쉘" imgSrc={체리쉘} />
+      </BaseFlavorSelect>
+      {/* <MenuSelector onSelectionChange={handleSelectionChange} />
       <div className="mt-5 h-0.5 w-full bg-[#F63F5D]"></div>
       <ItemSelector onSelectionChange={handleToppingSelectionChange} />
       <div className="mt-6 flex w-full justify-center">
@@ -93,7 +112,7 @@ export default function Home() {
         >
           완료
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
---
+++ src/components/home/BaseFlavorSelect.tsx
@@ -0,0 +1,121 @@
+import { BaseFlavor, BaseSize } from "@/app/types/common";
+import * as Select from "@radix-ui/react-select";
+import Image from "next/image";
+import React, { createContext, ReactNode, useContext, useState } from "react";
+
+interface SelectContextType {
+  selectedFlavor: keyof typeof BaseFlavor | null;
+  handleSelectFlavor: (option: keyof typeof BaseFlavor) => void;
+  selectedSize: keyof typeof BaseSize;
+  handleSelectSize: (option: keyof typeof BaseSize) => void;
+}
+
+const BaseFlavorSelectContext = createContext<SelectContextType | null>(null);
+
+interface BaseFlavorSelectProps {
+  children: ReactNode;
+  onSelectFlavor: (flavor: keyof typeof BaseFlavor) => void;
+  onSelectSize: (size: keyof typeof BaseSize) => void;
+}
+
+interface BaseFlavorOptionProps {
+  flavor: keyof typeof BaseFlavor;
+  imgSrc: string;
+}
+
+const BaseFlavorSelect = ({
+  children,
+  onSelectFlavor,
+  onSelectSize,
+}: BaseFlavorSelectProps) => {
+  const [selectedFlavor, setSelectedFlavor] = useState<
+    keyof typeof BaseFlavor | null
+  >(null);
+  const [selectedSize, setSelectedSize] =
+    useState<keyof typeof BaseSize>("1인 150g");
+
+  const handleSelectFlavor = (flavor: keyof typeof BaseFlavor) => {
+    setSelectedFlavor(flavor);
+    onSelectFlavor(flavor);
+  };
+
+  const handleSelectSize = (size: keyof typeof BaseSize) => {
+    setSelectedSize(size);
+    onSelectSize(size);
+  };
+
+  return (
+    <BaseFlavorSelectContext.Provider
+      value={{
+        selectedFlavor,
+        handleSelectFlavor,
+        selectedSize,
+        handleSelectSize,
+      }}
+    >
+      <div className="flex flex-col gap-2 py-3">{children}</div>
+    </BaseFlavorSelectContext.Provider>
+  );
+};
+
+const SelectItem = React.forwardRef<HTMLDivElement, Select.SelectItemProps>(
+  ({ children, ...props }, forwardedRef) => {
+    return (
+      <Select.Item
+        className={`cursor-pointer select-none rounded-md p-2 text-[#A3A3A3] transition-all hover:bg-[#F63F5D] hover:text-white data-[highlighted]:outline-none`}
+        {...props}
+        ref={forwardedRef}
+      >
+        <Select.ItemText>{children}</Select.ItemText>
+      </Select.Item>
+    );
+  },
+);
+
+const BaseFlavorOption = ({ flavor, imgSrc }: BaseFlavorOptionProps) => {
+  const context = useContext(BaseFlavorSelectContext);
+
+  if (!context) {
+    throw new Error("Cannot use BaseFlavorOption outside of BaseFlavorSelect");
+  }
+
+  const { selectedFlavor, handleSelectFlavor, selectedSize, handleSelectSize } =
+    context;
+  const isSelectedFlavor = selectedFlavor === flavor;
+
+  return (
+    <div
+      className={`flex cursor-pointer items-center gap-5 rounded-[20px] border border-[#F63F5D] py-3 pl-5 ${isSelectedFlavor && "bg-[#F63F5D] text-white"}`}
+      onClick={() => handleSelectFlavor(flavor)}
+    >
+      <Image src={imgSrc} alt={flavor} className="h-[50px] w-[50px]" />
+      <div className="flex flex-col justify-center">
+        <span className="text-xl font-semibold">{flavor}</span>
+        <Select.Root value={selectedSize} onValueChange={handleSelectSize}>
+          <Select.Trigger asChild>
+            <span
+              className={`cursor-pointer underline underline-offset-4 ${isSelectedFlavor ? "text-white" : "text-[#A3A3A3]"}`}
+            >
+              <Select.Value aria-label={selectedSize}>
+                {selectedSize}
+              </Select.Value>
+            </span>
+          </Select.Trigger>
+          <Select.Content
+            className="mt-2 rounded-lg border border-[#F63F5D] bg-white shadow-lg"
+            position="popper"
+          >
+            <SelectItem value="1인 150g">1인 150g</SelectItem>
+            <SelectItem value="2인 300g">2인 300g</SelectItem>
+            <SelectItem value="3인 450g">3인 450g</SelectItem>
+            <SelectItem value="4인 600g">4인 600g</SelectItem>
+          </Select.Content>
+        </Select.Root>
+      </div>
+    </div>
+  );
+};
+
+BaseFlavorSelect.Option = BaseFlavorOption;
+
+export default BaseFlavorSelect;
