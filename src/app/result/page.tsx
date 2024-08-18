381a4cdd2db8177c7e38f2f81ed4439cc1245380
2wndrhs <2wndrhs@gmail.com>
2024-08-19 01:35:27 +0900

    refactor: `BaseFlavor`, `BaseSize`, `ToppingType` 타입 분리

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -9,31 +9,6 @@ import axios from "axios";
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
-const BaseFlavor: { [key: string]: string } = {
-  요거트아이스크림: "PLAIN",
-  초코쉘: "CHOCO",
-  체리쉘: "CHERRY",
-};
-
-const BaseSize: { [key: string]: string } = {
-  "1인 150g": "SERVING_1",
-  "2인 300g": "SERVING_2",
-  "3인 450g": "SERVING_3",
-  "4인 600g": "SERVING_4",
-};
-
-const ToppingType: { [key: string]: string } = {
-  벌집꿀: "HONEY",
-  그래놀라: "GRANOLA",
-  초코링: "CHOCO_RING",
-  망고: "MANGO",
-  블루베리: "BLUEBERRY",
-  바나나: "BANANA",
-  복숭아: "PEACH",
-  샤인머스캣: "GRAPE",
-  멜론: "MELON",
-};
-
 export default function Result() {
   const searchParams = useSearchParams();
 
---
+++ src/app/types/common.ts
@@ -0,0 +1,24 @@
+export const BaseFlavor = {
+  요거트아이스크림: "PLAIN",
+  초코쉘: "CHOCO",
+  체리쉘: "CHERRY",
+} as const;
+
+export const BaseSize = {
+  "1인 150g": "SERVING_1",
+  "2인 300g": "SERVING_2",
+  "3인 450g": "SERVING_3",
+  "4인 600g": "SERVING_4",
+} as const;
+
+export const ToppingType = {
+  벌집꿀: "HONEY",
+  그래놀라: "GRANOLA",
+  초코링: "CHOCO_RING",
+  망고: "MANGO",
+  블루베리: "BLUEBERRY",
+  바나나: "BANANA",
+  복숭아: "PEACH",
+  샤인머스캣: "GRAPE",
+  멜론: "MELON",
+} as const;
